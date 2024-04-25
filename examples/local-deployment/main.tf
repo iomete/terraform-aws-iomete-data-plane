3cd8abd78261cff73266a21eb6deec40d5180b77
vusal-iom <vusal@iomete.com>
2024-04-25 15:39:46 +0200

    A new file "main.tf" was added for facilitating local test deployments.

---
+++ examples/local-deployment/main.tf
@@ -0,0 +1,23 @@
+module "data-plane-aws" {
+  #  source  = "iomete/data-plane-aws/aws"
+  #  version = "2.2.0"
+  source                    = "../.." # for local testing
+
+  # AWS region where cluster will be created
+  region                    = "us-east-1"
+
+  # A bucket name for IOMETE lakehouse. It should be unique withing compatible with AWS naming conventions.
+  lakehouse_bucket_name     = "iom-test-lake1"
+
+  # Cluster name. EKS cluster and other resource names will be prefixed with this name.
+  cluster_name              = "test-deployment1"
+}
+
+#################
+# Outputs
+#################
+
+output "cluster_name" {
+  description = "Kubernetes cluster name"
+  value       = module.data-plane-aws.cluster_name
+}
