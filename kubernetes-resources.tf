4196c5c05629fa18a0496792f155dfb148e04267
vusal-iom <vusal@iomete.com>
2024-04-27 18:20:56 +0200

    Refactor authentication in Kubernetes Terraform config
    This update removes previous exec based authentication with AWSCLI in Kubernetes and Helm providers. Instead, it now uses the AWS EKS cluster auth data source to set the access token directly. This change simplifies the token setup and removes the need for the AWSCLI tool locally.

--- kubernetes-resources.tf
+++ kubernetes-resources.tf
@@ -1,27 +1,19 @@
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_name
+}
+
 # Description: Creates a secret in Kubernetes details for the iomete-controller
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
+    token                  = data.aws_eks_cluster_auth.eks.token
   }
 }
 
