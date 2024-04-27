4cc0ff441fc45a8edcb9e9131868e10f4dd7d547
vusal-iom <vusal@iomete.com>
2024-04-27 19:23:09 +0200

    Update KMS key access comments in Terraform config
    The comments explaining KMS key access in the Terraform configuration for EBS volumes have been updated for improved clarity. The new comments better explain why access to the KMS key is required, how EBS encryption works, and how permissions are set up for decryption operations when using AWS EKS.

--- ebs-kms-permissions.tf
+++ ebs-kms-permissions.tf
@@ -1,10 +1,13 @@
 ################################################################################
-# KMS key access?
+# KMS Key Access
 ################################################################################
-# If region EBS is enabled, we need access to the KMS key. 
-# This is because the EBS CSI driver will create encrypted volumes, and we need to be able to decrypt them.
-# If region EBS is not enabled, you can skip this section.
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/csi-iam-role.html
+# When using Region EBS, access to the KMS key becomes necessary because the EBS CSI driver
+# encrypts the volumes it creates. To ensure these volumes function correctly, AWS EKS requires
+# the ability to decrypt them. Therefore, we must set up the necessary permissions to
+# access the KMS key for decryption operations.
+# If Region EBS is not enabled, you can skip this configuration.
+# For more details on setting up the CSI IAM role, see:
+# https://docs.aws.amazon.com/eks/latest/userguide/csi-iam-role.html
 
 resource "aws_iam_policy_attachment" "kms_describe_attachment" {
   count      = var.kms_key_arn != "null" ? 1 : 0
